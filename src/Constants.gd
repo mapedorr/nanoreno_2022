2ecca6b67018d8e4e86246c08ee2b32af918421f
carenalga <mapedorr@gmail.com>
2022-04-01 18:50:21 -0500

    The game can be finished for both endings.
    Updated assets to use the UI style defined by Sandra (a.k.a. Samitha).
    Created all the Timelines with Spanish texts.
    Added Francisco to the cast.
    === Ready to work on the main menu and credits ===

---
+++ src/Constants.gd
@@ -0,0 +1,50 @@
+extends Node
+
+const Intro := [
+	'1996',
+	'Álvaro Mendoza, el terrateniente y gobernador.',
+	'Sacó corriendo a muchos de Sagrado, el pueblo donde vivían Ramiro, Cecilia, Marta y Joaquín.',
+	'Todo para quedarse con esas tierras y ponerlas al servicio de los ganaderos y los traficantes de coca.',
+	'...',
+	'2006'
+]
+const Past := [
+	'Leyla bebió lentamente y empezó a sentir un cosquilleo por todo el cuerpo tan pronto como el líquido tocó su lengua.',
+	'Su cuerpo ya no era su cuerpo, y su alma, poco a poco, se alejaba de él.',
+	'Ahora se veía, y veía a Marta y su carro de chucherías.',
+	'Se sentía tranquila, segura de lo que estaba pasando mientras la oscuridad se apoderaba de todo.',
+	'Cuando la oscuridad se disipó, se encontraba en la misma calle, pero algo había cambiado.',
+	'Su ropaje era distinto, y junto a ella caminaba una mujer con ropas elegantes.',
+	'...',
+	'1813'
+]
+const Murder := [
+	'Dos días después, Isabela y Carmen salieron de la hacienda con una bolsa de tela con el dinero que debía pagarse a los matones.',
+	'Isabela estaba muy emocionada, mientras que Carmen no dejaba de decirle que era una locura.',
+	'Cuando llegaron al campo de Uchuvas vieron a un hombre. Este les preguntó por el dinero.',
+	'Carmen le entregó la bolsa y el hombre, después de contarlo, sonrío y le dijo que no debía preocuparse por la vida de su tonto hermano.',
+	'Isabela le dijo al hombre que lo que hacían estaba muy mal.',
+	'Luego sintió un golpe en la nuca y perdió el conocimiento.',
+]
+const Promise := [
+	'2006',
+	'Leyla estaba sentada, aún confundida por lo que había visto.',
+	'Marta la miraba fijamente.',
+	'¿Por fin había llegado el día en el que dos vidas arruinadas por la ambición de una pareja cobrarían su venganza?',
+]
+const Revenge := [
+	'Marta sacó un puñal y se lo clavó a Leyla en el corazón.',
+	'El grito de Leyla quedó apagado por la bola de tela que Marta le había puesto en la boca.',
+	'Marta tomó el cuerpo de Leyla y lo subió a su carro.',
+	'Luego arrancó el motor y se alejo.',
+	'...',
+	'FIN'
+]
+const Leaving := [
+	'Marta le tiró a Leyla un cofre de metal y se alejó en su coche.',
+	'Cuando Leyla recuperó su cuerpo tomó el cofre y lo abrió.',
+	'En él estaban la fotografía de Marta y su familia y un par de semillas.',
+	'Leyla nunca supo nada más de Marta.',
+	'...',
+	'FIN'
+]
--- src/Main.gd
+++ src/Main.gd
@@ -3,35 +3,29 @@ extends Node2D
 
 signal continued
 
+const Constants := preload('Constants.gd')
+
 var _animating := false
+var _timelines_json := {}
 
 onready var _continue: TextureButton = find_node('Continue')
 onready var _label: Label = find_node('Label')
+onready var _next_indicator: TextureRect = find_node('NextIndicator')
+onready var _next_animation: AnimationPlayer = _next_indicator.get_node('AnimationPlayer')
 
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Godot methods ░░░░
 func _ready() -> void:
+	_label.text = ''
 	_label.modulate.a = 0.0
 	
 	_continue.connect('pressed', self, '_next_message')
 	
-	yield(get_tree().create_timer(0.5), 'timeout')
+#	yield(get_tree().create_timer(0.5), 'timeout')
+#	goto_timeline('Test', 'Test')
 	
-	yield(run(
-		[
-			'Érase una vez',
-			'En la capital de un país del tercer mundo',
-			'La historia que conectaba al pasado y al presente',
-			'En una repetición sin fin',
-			'...',
-			'1815'
-		]
-	), 'completed')
-	
-	var new_dialog = Dialogic.start('TestPast')
-	new_dialog.connect('timeline_end', self, '_goto_present')
-	
-	add_child(new_dialog)
+	yield(run(Constants.Intro), 'completed')
+	goto_timeline('Meeting', 'Past')
 
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ public methods ░░░░
@@ -46,23 +40,32 @@ func run(commands: Array) -> void:
 	yield(get_tree(), 'idle_frame')
 
 
+func goto_timeline(timeline: String, next_timeline := '') -> void:
+	var new_dialog = Dialogic.start(timeline)
+	
+	if next_timeline:
+		new_dialog.connect('timeline_end', self, '_timeline_ended', [next_timeline])
+
+	add_child(new_dialog)
+
+
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ private methods ░░░░
 func _next_message() -> void:
-	if _animating: return
+	if _animating:
+		$Tween.remove_all()
+
+		_label.modulate.a = 1.0
+		_next_animation.play('UpDown')
+		_animating = false
+		
+		return
 	
-	_animating = true
-	
-	$Tween.interpolate_property(
-		_label, 'modulate:a',
-		1.0, 0.0,
-		0.9, Tween.TRANS_CUBIC, Tween.EASE_OUT
-	)
-	$Tween.start()
-	yield($Tween, 'tween_all_completed')
+	_next_animation.play('RESET')
 	
 	_animating = false
 	_label.text = ''
+	_label.modulate.a = 0.0
 	emit_signal('continued')
 
 
@@ -73,18 +76,30 @@ func _show_message(msg: String) -> void:
 	$Tween.interpolate_property(
 		_label, 'modulate:a',
 		0.0, 1.0,
-		1.2, Tween.TRANS_CUBIC, Tween.EASE_OUT
+		1.5, Tween.TRANS_CUBIC, Tween.EASE_OUT
 	)
 	$Tween.start()
 	yield($Tween, 'tween_all_completed')
 	
+	_next_animation.play('UpDown')
 	_animating = false
 
 
-func _goto_present(timeline_name: String) -> void:
-	yield(run(['2006']), 'completed')
-	
-	var new_dialog = Dialogic.start('Test')
-#	new_dialog.connect('timeline_end', self, '_goto_present')
-	
-	add_child(new_dialog)
+func _timeline_ended(timeline_json: String, next_timeline := '') -> void:
+	prints('---', DialogicUtil.get_timeline_dict()[timeline_json].name)
+	match DialogicUtil.get_timeline_dict()[timeline_json].name:
+		'Vision':
+			yield(run(Constants.Past), 'completed')
+			goto_timeline(next_timeline, 'Stake')
+		'Past':
+			yield(run(Constants.Murder), 'completed')
+			goto_timeline(next_timeline, 'Promise')
+		'Stake':
+			yield(run(Constants.Promise), 'completed')
+			goto_timeline(next_timeline, 'xxx')
+		'Revenge':
+			yield(run(Constants.Revenge), 'completed')
+			# TODO: volver al menú principal
+		'Forgiveness':
+			yield(run(Constants.Leaving), 'completed')
+			# TODO: volver al menú principal
